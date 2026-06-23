dbb76eac1805dfcd130caaf38daab13d8d305fe2
melita2024 <kelongmelita80@gmail.com>
2026-06-23 15:12:46 +0100

    feat: add MicCheck component to interview generation page

--- app/(root)/interview/page.tsx
+++ app/(root)/interview/page.tsx
@@ -1,4 +1,5 @@
 import Agent from "@/components/Agent";
+import MicCheck from "@/components/MicCheck";
 import { getCurrentUser } from "@/lib/actions/auth.action";
 
 const Page = async () => {
@@ -7,6 +8,9 @@ const Page = async () => {
   return (
     <>
       <h3>Interview generation</h3>
+
+      <MicCheck />
+
 
       <Agent userName={user?.name!} userId={user?.id} type="generate" userProfilePhoto={user?.profilePhoto} />
     </>
